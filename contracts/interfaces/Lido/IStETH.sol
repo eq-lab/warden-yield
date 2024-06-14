fe4f88a689d48c03c2553d3427b96583bd4d3ee1
Pavel <pavel.smelov@equilibrium.io>
2024-06-14 14:56:56 +0300

    lido interactor

---
+++ contracts/interfaces/Lido/IStETH.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity =0.8.26;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+/// @title Interface for StETH
+interface IStETH is IERC20 {
+  function submit(address referral) external payable returns (uint256);
+}
