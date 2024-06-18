ca3b9dbc646f32ccb5d8b3f44b5aacd7641fcaf8
Pavel <pavel.smelov@equilibrium.io>
2024-06-18 16:53:51 +0300

    add user balance checks

--- test/int/Lido.spec.ts
+++ test/int/Lido.spec.ts
@@ -12,6 +12,7 @@ describe('LidoYield', () => {
     const [_, user] = await ethers.getSigners();
 
     const wStEthBalanceBefore = await wStEth.balanceOf(lidoYield.getAddress());
+    const userEthBalanceBefore = await user.provider.getBalance(user.address);
 
     const input = parseEther('1');
     await lidoYield.connect(user).stake(input, { value: input });
@@ -20,10 +21,12 @@ describe('LidoYield', () => {
 
     const wStEthBalanceAfter = await wStEth.balanceOf(lidoYield.getAddress());
     const wStEthBalanceDelta = wStEthBalanceAfter - wStEthBalanceBefore;
+    const userEthBalanceAfter = await user.provider.getBalance(user.address);
 
     expect(wStEthBalanceDelta).to.be.greaterThan(0);
     expect(await lidoYield.totalStakedAmount()).to.be.eq(wStEthBalanceDelta);
     expect(await lidoYield.userStakedAmount(user.address)).to.be.eq(wStEthBalanceDelta);
+    expect(userEthBalanceBefore - userEthBalanceAfter).to.be.gte(input);
   });
 
   it('user stake, weth', async () => {
@@ -31,9 +34,9 @@ describe('LidoYield', () => {
     const [_, user] = await ethers.getSigners();
 
     const wStEthBalanceBefore = await wStEth.balanceOf(lidoYield.getAddress());
-
     const input = parseEther('1');
     await setTokenBalance(await weth9.getAddress(), user.address, input);
+    const userWethBalanceBefore = await weth9.balanceOf(user.address);
     await weth9.connect(user).approve(lidoYield.target, input);
 
     await lidoYield.connect(user).stake(input);
@@ -42,9 +45,11 @@ describe('LidoYield', () => {
 
     const wStEthBalanceAfter = await wStEth.balanceOf(lidoYield.getAddress());
     const wStEthBalanceDelta = wStEthBalanceAfter - wStEthBalanceBefore;
+    const userWethBalanceAfter = await weth9.balanceOf(user.address);
 
     expect(wStEthBalanceDelta).to.be.greaterThan(0);
     expect(await lidoYield.totalStakedAmount()).to.be.eq(wStEthBalanceDelta);
     expect(await lidoYield.userStakedAmount(user.address)).to.be.eq(wStEthBalanceDelta);
+    expect(userWethBalanceBefore - userWethBalanceAfter).to.be.eq(input);
   });
 });
