b0f96d8aa49eb0ed7c0913cf867c3a075b524221
Overseven <igor.b@equilibrium.io>
2026-02-25 10:46:09 +0300

    init AaveYieldV2 contract

---
+++ contracts/AaveYieldV2.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.26;
+
+import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol';
+
+import './libraries/Errors.sol';
+import './interactors/AaveInteractor.sol';
+import './YieldStorage.sol';
+import './interfaces/IAaveYield.sol';
+
+contract AaveYieldV2 is UUPSUpgradeable, Ownable2StepUpgradeable, AaveInteractor, YieldStorage, IYieldBase {
+  /// @dev method called during the contract upgrade
+  function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+  function withdraw(address token) external returns (uint256 withdrawAmount) {
+    withdrawAmount = userStakedAmount(msg.sender, token);
+    if (withdrawAmount == 0) revert Errors.ZeroAmount();
+    _removeStake(msg.sender, token);
+    _aaveWithdraw(token, withdrawAmount);
+
+    emit Withdraw(msg.sender, token, withdrawAmount);
+  }
+
+  /// @dev returns current user balance available to withdraw
+  /// @param user user address
+  /// @param underlyingToken underlying token address
+  /// @return availableToWithdraw available to withdraw amount
+  function getUserUnderlyingAmount(
+    address user,
+    address underlyingToken
+  ) public view returns (uint256 availableToWithdraw) {
+    uint256 scaledDeposit = userShares(user, underlyingToken);
+    availableToWithdraw = _getBalanceFromScaled(scaledDeposit, underlyingToken);
+  }
+}
