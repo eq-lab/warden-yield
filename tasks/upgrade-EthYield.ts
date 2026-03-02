0231cc3193eab0200b4f1c5353204ea8f18aac87
Overseven <igor.b@equilibrium.io>
2026-03-02 13:31:57 +0300

    fix task

--- tasks/upgrade-EthYield.ts
+++ tasks/upgrade-EthYield.ts
@@ -1,21 +1,16 @@
 import { task } from 'hardhat/config';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { loadDeployConfig } from '../deploy/config';
-import { deployWardenYield, upgradeAaveYieldToV2, upgradeEthYield } from '../deploy/src';
+import { upgradeEthYield } from '../deploy/src';
 
 interface UpgradeArgs {
   networkName: string;
-  proxyAddress: string;
-  newImplementationAddress: string;
-  token: string;
   creatorPrivateKey: string;
 }
 
 task('task:upgrade-eth-yield', 'Upgrade EthYield from v1 to v2')
   .addParam<string>('networkName', 'Network name')
   .addParam<string>('creatorPrivateKey', 'Private key of contracts creator')
-  // .addOptionalParam<number>('gasLimit', 'Gas limit')
-  // .addOptionalParam<number>('gasPrice', 'Gas price')
   .setAction(async (taskArgs: UpgradeArgs, hre: HardhatRuntimeEnvironment) => {
     const dryRun =
       hre.config.networks.hardhat.forking !== undefined ? hre.config.networks.hardhat.forking.enabled : false;
@@ -24,22 +19,18 @@ task('task:upgrade-eth-yield', 'Upgrade EthYield from v1 to v2')
     if (!dryRun && hre.network.name.toLowerCase() !== network) {
       throw new Error(`The network from the config and from CLI "--network" must be same!`)
     }
-    if (taskArgs.token !== 'usdc' && taskArgs.token !== 'usdt') {
-      throw new Error(`Token must be set as 'usdt' or 'usdc'`);
-    }
 
     if (dryRun) {
       console.log(`Dry run command on fork`);
       const blockNumber = await hre.ethers.provider.getBlockNumber();
       console.log(`Fork block number: ${blockNumber}`);
     }
-
+    const signer = new hre.ethers.Wallet(taskArgs.creatorPrivateKey, hre.ethers.provider);
     const config = await loadDeployConfig(network, signer.provider, dryRun);
     if (config.ethYield?.lidoWithdrawalQueue === undefined) {
       throw new Error('Unknown lidoWithdrawalQueue address');
     }
-
-    const signer = new hre.ethers.Wallet(taskArgs.creatorPrivateKey, hre.ethers.provider);
+    
     const balanceBefore = await signer.provider.getBalance(signer.address);
     console.log(`Balance before: ${hre.ethers.formatEther(balanceBefore)} Eth\n`);
     
